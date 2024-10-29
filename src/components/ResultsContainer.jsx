f1d3d9f831deda63c51b26a223ff9c711452676c
Josh <119835493+josh76543210@users.noreply.github.com>
2024-10-28 22:13:05 -0700

    Add top border to results numbers box;

--- src/components/ResultsContainer.jsx
+++ src/components/ResultsContainer.jsx
@@ -1,13 +1,13 @@
 function ResultsContainer() {
   return (
-    <div className="bg-slate900 text-white py-8 px-4 lg:rounded-bl-xxl">
+    <div className="bg-slate900 text-white py-8 px-5 lg:rounded-bl-xxl">
       <h2 className="font-bold text-xl mb-4">Your results</h2>
-      <p className="text-slate300 mb-4">
+      <p className="text-slate300 mb-5">
         Your results are shown below based on the information you provided. To
         adjust the results, edit the form and click &quot;calculate
         repayments&quot; again.
       </p>
-      <div className="bg-slate950 py-4 px-3">
+      <div className="bg-slate950 py-4 px-3 rounded-lg border-t-4 border-lime">
         <div className=" border-b border-slate700 mb-4">
           <p className="text-slate300 mb-2">Your monthly repayments</p>
           <p className="mb-4 text-4xl font-bold text-lime">$1,797.74</p>
