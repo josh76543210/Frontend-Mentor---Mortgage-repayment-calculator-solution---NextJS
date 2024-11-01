bb068ba687225c6ccfa405b1962c0c46572dc33c
Josh <119835493+josh76543210@users.noreply.github.com>
2024-10-31 21:43:51 -0700

    Add padding to page on larger screens;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="font-PlusJakartaSans text-slate900 bg-slate100 h-full min-h-screen flex justify-center items-center flex-col">
+    <div className="font-PlusJakartaSans text-slate900 bg-slate100 h-full min-h-screen flex justify-center items-center flex-col 2sm:pt-8">
       <div className="bg-white rounded-b-xl 2sm:rounded-3xl overflow-hidden max-w-xl lg:flex lg:max-w-4xl mx-auto shrink-0 mt-0 2sm:mt-auto">
         <CalculatorContainer />
         <ResultsContainer />
